1d52c6b1ecb3b8761b524f5895917918dfdcb1ca
julianyedro-code <julian.yedro@davinci.edu.ar>
2025-10-15 00:07:15 -0300

    metodos de la clase GerenteRegional

--- Jumbox/src/GerenteRegional.java
+++ Jumbox/src/GerenteRegional.java
@@ -7,6 +7,51 @@ public class GerenteRegional extends Usuario{
 
 	   private ArrayList<Pedido> pedidosPendientesAprobacion;
 
+	public GerenteRegional() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public GerenteRegional(int idUsuario, String nombre, String dni, String email, Direccion direccion, String telefono,
+			String contrasenia, boolean activo, ArrayList<Notificacion> notificaciones) {
+		super(idUsuario, nombre, dni, email, direccion, telefono, contrasenia, activo, notificaciones);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrayList<Pedido> getPedidosPendientesAprobacion() {
+		return pedidosPendientesAprobacion;
+	}
+
+	public void setPedidosPendientesAprobacion(ArrayList<Pedido> pedidosPendientesAprobacion) {
+		this.pedidosPendientesAprobacion = pedidosPendientesAprobacion;
+	}
+	
+
+
+    public ArrayList<Usuario> visualizarEmpleados() {
+     
+    	return new ArrayList<>();
+    }
+
+    public boolean aprobarTraspasos(Pedido pedido) {
+        if (pedidosPendientesAprobacion != null) {
+            pedidosPendientesAprobacion.remove(pedido);
+        }
+        return true;
+    }
+
+    public ArrayList<Sucursal> consultarStockSucursalesNac() {
+        
+    	return new ArrayList<>();
+    }
+
+    public void agregarSucursal(Sucursal sucursal) {
+        
+    }
+
+    public ArrayList<Pedido> obtenerPedidosPendientes() {
+        return pedidosPendientesAprobacion;
+    }
 	    
 	
 	
